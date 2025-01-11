67286b3c08d39b326f47fcabff6367de24354452
Tish0 <tishotodorow92@gmail.com>
2025-01-11 09:34:36 +0200

    Update pen4oWeek1.js

--- Pen4o’s Odyssey/pen4oWeek1.js
+++ Pen4o’s Odyssey/pen4oWeek1.js
@@ -2,11 +2,10 @@ function pen4oWeek1(a, b, c) {
 
     if (a < b + c && b < a + c && c < a + b) {
         console.log(`The Triangle of Truth with sides ${a}, ${b}, and ${c} has been validated. Pen4o, you may begin your journey!`);    
-    } else if (a > c - b || b > c - a || c > b - a) {
+    } else {
         console.log(`The Triangle of Truth with sides ${a}, ${b}, and ${c} is invalid. Pen4o remains stuck!`);  
     }
 
 }
 
 pen4oWeek1(3, 4, 5);
-pen4oWeek1(1, 2, 3);
