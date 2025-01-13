ef2c5450fcc4005de5b7ec9c5f843185f7d69b35
Tish0 <tishotodorow92@gmail.com>
2025-01-13 23:43:37 +0200

    Delete First Steps In Coding - Lab directory

--- First Steps In Coding - Lab/inchesToCentimeters.js
+++
@@ -1,6 +0,0 @@
-function inchesToCentimeters(arg) {
-    let a = arg * 2.54;
-    console.log(a);
-}
-
-inchesToCentimeters(7);
--- First Steps In Coding - Lab/squareArea.js
+++
@@ -1,6 +0,0 @@
-function squareArea(a) {
-    let area = a;
-    console.log(a * a);
-}
-
-squareArea(5);
